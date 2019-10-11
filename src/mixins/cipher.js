28340c88e6d604c4ce328241a929f60a5a3fb99c
Erik Uy <eriuy@publicisgroupe.net>
2019-10-11 16:31:34 +1100

    cipher mixin

--- src/mixins/cipher.js
+++ src/mixins/cipher.js
@@ -1,7 +1,15 @@
+import SimpleCrypto from "simple-crypto-js"; 
+var _secretKey='imalittleteapotshortandstout'
+
 export const cipherMixin = {
+    data(){
+        return {
+            crypto:new SimpleCrypto(_secretKey)
+        };
+    },
     methods: {
         decrypt(ciphertext, key) {
-
+            this.crypto
         },
         encrypt(plaintext, key){
 
